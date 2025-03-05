093f7d49314dfa78d5ac47b9370a25f27f2de131
Jean Gomes <jeangp2006@gmail.com>
2025-03-05 19:56:04 -0300

    feat: fix regex

--- app/Filament/Resources/CreditCardBillResource/Pages/CreateCreditCardBill.php
+++ app/Filament/Resources/CreditCardBillResource/Pages/CreateCreditCardBill.php
@@ -30,14 +30,22 @@ class CreateCreditCardBill extends CreateRecord
         foreach ($linhas as $linha) {
             //preg_match('/^(\d{2} \w{3}) (.+?)(?: - (\d+\/\d+))? (\d+,\d{2})$/', $linha, $matches);
             // Expressão regular ajustada
-            preg_match('/^(\d{2} \w{3}) (.+?)(?: - (\d+\/\d+))? (\d+,\d{2})$/', $linha, $matches);
+            //preg_match('/^(\d{2} \w{3}) (.+?)(?: - (\d+\/\d+))? (\d+,\d{2})$/', $linha, $matches);
+            preg_match('/^(\d{2} \p{L}+) (.+?)\s*(?:- (\d+\/\d+))?\s*([\d.,]+)$/u', $linha, $matches);
+
 
             if ($matches) {
+                $amount = floatval(str_replace(['.', ','], ['', '.'], $matches[4]));
+
                 $resultado[] = [
-                    'transaction_date' => $matches[1], // Data no formato "DD MMM"
+                    'transaction_date' => $this->setTransactionDate($matches[1]), // Data no formato "DD MMM"
                     'description' => trim($matches[2]), // Descrição
                     'parcelas' => $matches[3] ?? null, // Parcelas (ex.: "1/3"), se existirem
-                    'amount' => floatval(str_replace(',', '.', $matches[4])), // Valor como float
+                    'amount' => $amount, // floatval(str_replace(',', '.', $matches[4])), // Valor como float
+                    'individual_expense' => true,
+                    'common_expense' => false,
+                    'owner_expense' => 'D',
+                    'who_paid'=> 'D'
                 ];
             }
         }
@@ -58,5 +66,30 @@ class CreateCreditCardBill extends CreateRecord
         //dd($this->data,$this->record->exists,$this->record->id);
         // Runs after the form fields are saved to the database.
        // dd('salvou');
+    }
+
+    private function setTransactionDate($value): string
+    {
+        $monthNames = [
+            'JAN' => '01',
+            'FEV' => '02',
+            'MAR' => '03',
+            'ABR' => '04',
+            'MAI' => '05',
+            'JUN' => '06',
+            'JUL' => '07',
+            'AGO' => '08',
+            'SET' => '09',
+            'OUT' => '10',
+            'NOV' => '11',
+            'DEZ' => '12',
+        ];
+
+        // Assume que o valor seja uma string como '04 ABR'
+        preg_match('/(\d{2}) (\w{3})/', $value, $matches);
+        $day = $matches[1];
+        $month = $monthNames[strtoupper($matches[2])];
+        $year = date('Y'); // Ano atual, ou pode ser um ano específico
+        return "{$year}-{$month}-{$day}";
     }
 }
