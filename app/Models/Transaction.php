b09041416139f09a5eb36dd3123260fecb0c6930
Jean Gomes <jeangp2006@gmail.com>
2025-04-06 20:45:43 -0300

    feat: compute creditCardBill fields (common_amount, individual_amount) after updated transactions

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property-read CreditCardBill $creditCardBill
+ */
 class Transaction extends Model
 {
     protected $fillable = [
@@ -33,28 +36,18 @@ class Transaction extends Model
         return $this->belongsTo(CreditCardBill::class);
     }
 
-/*    public function setTransactionDateAttribute($value): void
+    protected static function booted(): void
     {
-        $monthNames = [
-            'JAN' => '01',
-            'FEV' => '02',
-            'MAR' => '03',
-            'ABR' => '04',
-            'MAI' => '05',
-            'JUN' => '06',
-            'JUL' => '07',
-            'AGO' => '08',
-            'SET' => '09',
-            'OUT' => '10',
-            'NOV' => '11',
-            'DEZ' => '12',
-        ];
-
-        // Assume que o valor seja uma string como '04 ABR'
-        preg_match('/(\d{2}) (\w{3})/', $value, $matches);
-        $day = $matches[1];
-        $month = $monthNames[strtoupper($matches[2])];
-        $year = date('Y'); // Ano atual, ou pode ser um ano específico
-        $this->attributes['transaction_date'] = "{$year}-{$month}-{$day}";
-    }*/
+        static::updated(function (Transaction $transaction) {
+            $transaction->load(['creditCardBill']);
+            $creditCardBill = $transaction->creditCardBill;
+            $creditCardBill->common_amount = $creditCardBill->transactions()
+                ->where('common_expense', '=', 1)
+                ->sum('amount');
+            $creditCardBill->individual_amount = $creditCardBill->transactions()
+                ->where('individual_expense', '=', 1)
+                ->sum('amount');
+            $creditCardBill->save();
+        });
+    }
 }
