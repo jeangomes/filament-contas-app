bfa9a7baa856d3adf3810c38bdd5f491ac5f9c42
Jean Gomes <jeangp2006@gmail.com>
2025-11-16 19:03:51 -0300

    filament resource users

---
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Filament\Resources\Users\Schemas;
+
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class UserForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->label('Nome')
+                    ->required(),
+                TextInput::make('email')
+                    ->label('Email')
+                    ->email()
+                    ->required(),
+                DateTimePicker::make('email_verified_at')
+                ->label('E-mail Verificado'),
+                TextInput::make('password')
+                    ->label('Senha')
+                    ->password()
+                    ->required(),
+            ]);
+    }
+}
